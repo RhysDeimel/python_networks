e2d735ae0675e6cc070f66d839ec0e06a04e56d5
Rhys Deimel <deimelr@hotmail.com>
2017-05-31 17:47:22 +1000

    added comments

--- servicename.py
+++ servicename.py
@@ -5,6 +5,9 @@ import socket
 # Ports 1024-49151 - user or registered ports
 # Ports >49151 - dynamic / private ports
 
+# database of well-known service names and port numbers usually kept in
+# /etc/servics on Linux and Mac OS X machines
+
 print(socket.getservbyname('domain'))  # port 53
 print(socket.getservbyname('whois'))  # port 43
 print(socket.getservbyname('smtp'))  # port 25
